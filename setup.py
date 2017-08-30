bf735b489b1db53f3562d8169220b23da9c1b234
Alessandro Viganò <alvistar@gmail.com>
2017-08-31 00:30:31 +0200

    Add build library instructions

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+from distutils.command.build_clib import build_clib
+
+libblake2b = ('blake2b', {'sources': ['blake2b-ref.c']})
 
 setup(
     name = "cuckooapp",
+    libraries = [libblake2b],
     ext_modules = cythonize('pycuckoo.pyx'),
 )
